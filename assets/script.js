178de06a7c22a6b8b97a953050944c4fe2982103
Minami Itsukaichi <mitukaichi@gmail.com>
2023-10-10 16:52:14 -0700

    add page that shows the details of the specific recipe

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 /// Base API request to get recipe data 
-var requestURL = "https://api.edamam.com/api/recipes/v2?type=public&q="+ "italian" + "&app_id=f77c7e0e&app_key=43e8d41a5b2ed56c8d6d782c1d900e3e";
-    fetch(requestURL)
+var requestUrl = "https://api.edamam.com/api/recipes/v2?type=public&q="+ "italian" + "&app_id=f77c7e0e&app_key=43e8d41a5b2ed56c8d6d782c1d900e3e";
+    fetch(requestUrl)
         .then(function (response) {
             return response.json();
         })
@@ -11,7 +11,7 @@ var requestURL = "https://api.edamam.com/api/recipes/v2?type=public&q="+ "italia
                 $("#recipe_results").append('<h3 id = "'+ recipeId + '">' + data.hits[i].recipe.label + '</h3>');
                 var recipeItemUl = $(recipeIdDocument).append("<ul></ul>"); 
                 recipeItemUl.append("<li>" + data.hits[i].recipe.cuisineType + "</li>")
-                recipeItemUl.append("<li>" + data.hits[i].recipe.calories + " calories</li>")
+                recipeItemUl.append("<li>" + Math.round(data.hits[i].recipe.calories) + " calories</li>")
                 recipeItemUl.append('<li><a href = "/food-finder/recipe_results.html?q=' + recipeId + '"> Details</a></li>')
                 recipeItemUl.append("<img src=" + data.hits[i].recipe.images.SMALL.url + ">")
             }
