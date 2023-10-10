76dc80ff8be50aaea5c565b146cd5bb730c82075
Minami Itsukaichi <mitukaichi@gmail.com>
2023-10-10 14:34:24 -0700

    switching the recipe API to Edamam

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,13 @@
 /// Base API request to get recipe data 
-var requestURL = "https://api.spoonacular.com/recipes/complexSearch?apiKey=bc252eb72b7f48ba812d2420ce79224d&q=" + "italian";
+var requestURL = "https://api.edamam.com/api/recipes/v2?type=public&q="+ "italian" + "&app_id=f77c7e0e&app_key=43e8d41a5b2ed56c8d6d782c1d900e3e";
     fetch(requestURL)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             for (i = 0; i < 5; i++){
-                console.log(data.results[i].id);
-                console.log(data.results[i].title);
-                console.log(data.results[i].image);
+                console.log(data.hits[i].recipe.label);
+                console.log(data.hits[i].recipe.cuisineType);
+                console.log(data.hits[i].recipe.image);
             }
-        }
-    );
+        });
