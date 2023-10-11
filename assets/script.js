368bef5b4d7e0c28e47134fe5a183c62a51fb3e6
Minami Itsukaichi <mitukaichi@gmail.com>
2023-10-10 20:30:51 -0700

    added the scenario where there is no matching recipe

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,18 @@
+$(document).ready(function(){   
+    var search = $("#search");
+    var button = $("button");
+    var query = "";
+
 /// Base API request to get recipe data 
 var requestURL = "https://api.edamam.com/api/recipes/v2?type=public&q="+ "italian" + "&app_id=f77c7e0e&app_key=43e8d41a5b2ed56c8d6d782c1d900e3e";
-    fetch(requestURL)
-        .then(function (response) {
+fetch(requestURL)
+    .then(function (response) {
             return response.json();
-        })
-        .then(function (data) {
+    })
+    .then(function (data) {
+        if (data.hits.length === 0) {
+            $("#recipe_results").text("There is no recipe matching your query. Search another menu.")
+        } else {
             for (i = 0; i < 3; i++){
                 var recipeId = data.hits[i].recipe.uri.split("_")[1];
                 var recipeIdDocument = "#" + recipeId;
@@ -15,4 +23,18 @@ var requestURL = "https://api.edamam.com/api/recipes/v2?type=public&q="+ "italia
                 recipeItemUl.append('<li><a href = /food-finder/recipe_results.html?q=' + recipeId + '> Details</a></li>')
                 recipeItemUl.append("<img src=" + data.hits[i].recipe.images.SMALL.url + ">")
             }
-        });
+        }
+    });
+
+    query = search.val();
+
+
+
+    button.on("click", function(event){
+        event.preventDefault();
+        query = search.val();
+        search.val("");
+    })
+
+
+});
