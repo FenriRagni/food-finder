59bd7cf8fcf8a438c6889545d1d4efc83593aa12
nathangero <nathangero@yahoo.com>
2023-10-11 08:32:11 -0700

    implement getting browser's location

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ## Resources
 
+[Browser Geolocation](https://www.w3schools.com/html/html5_geolocation.asp)
+
 ### Google Places
 
 [Google Autocomplete](https://www.youtube.com/watch?v=c3MjU9E9buQ)
--- assets/js/query-search.js
+++ assets/js/query-search.js
@@ -1,8 +1,16 @@
+const GOOGLE = "AIzaSyCFTg8yxhfKfqvVhtZpfmTyXco9qlHLm2Q";
+
 var queryItem = $("#query-item");
 var queryLocation = $("#query-location");
 var buttonSearch = $("#button-search");
-var autocomplete;
+var deviceLocation = { lat: 0, lng: 0 }
+var searchLocation = { lat: 0, lng: 0 }
 
+// Google services
+var gAutocomplete;
+var gPlaces;
+
+askForUserLocation()
 $(function() {
     buttonSearch.on("click", handleSearch);
 })
@@ -12,49 +20,90 @@ $(function() {
 function handleSearch(event) {
     event.stopPropagation();
     event.preventDefault();
-
-    let item = queryItem.val();
-    let location = queryLocation.val();
-    // console.log("item:", item, "location:", location);
-
-    console.log(buildLocationQuery(location))
+    
+    fetchGooglePlaces()
 }
 
-async function handlePlaceChanged() {
-    var place = autocomplete.getPlace();
+async function handleUpdateAutocomplete() {
+    var place = gAutocomplete.getPlace();
 
-    console.log(place)
     if (!place.geometry) { // Checks if the user did not click on a place
+        console.log(place)
         queryLocation.val("")
     } else {
-        console.log(place)
+        searchLocation.lat = place.geometry.location.lat()
+        searchLocation.lng = place.geometry.location.lng()
     }
 }
 
 
 async function fetchGooglePlaces() {
+    let location = new google.maps.LatLng(searchLocation.lat, searchLocation.lng);
+
     var request = {
-        query: 'Museum of Contemporary Art Australia',
-        fields: ['name', 'geometry'],
-      };
-    
-    
-      autocomplete.findPlaceFromQuery(request, function(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-            console.log("worked")
-        } else {
-            console.log("didn't work")
+        location: location,
+        radius: 5,
+        query: String(queryItem),
+        fields: ['formatted_address', 'name', 'geometry', 'opening_hours'],
+    };
+
+    gPlaces.textSearch(request, function(results, status) {
+        console.log("results:", results, "status:", status)
+        if (status !== google.maps.places.PlacesServiceStatus.OK) return 
+
+        console.log("findPlaceFromQuery:")
+        for (var i = 0; i < results.length; i++) {
+            // console.log(results[i])
+            if (results[i].name == String(queryItem)) {
+                console.log("found the place")
+            }
         }
       });
 }
 
-function initAutocomplete() {
-    autocomplete = new google.maps.places.Autocomplete(document.getElementById("query-location"), {
-        types: ["(cities)"],
-        fields: ["geometry", "name", "place_id",],
-        strictBounds: true,
-    });
 
-    autocomplete.addListener('place_changed', handlePlaceChanged);
+function initGoogle() {
+    initAutocomplete()
+    initPlaces()
 }
 
+
+function initAutocomplete() {
+        // Create a bounding box with sides ~10km away from the center point
+        const defaultBounds = {
+            north: deviceLocation.lat + 0.1,
+            south: deviceLocation.lat - 0.1,
+            east: deviceLocation.lng + 0.1,
+            west: deviceLocation.lng - 0.1,
+        };
+    
+        const options = {
+            bounds: defaultBounds,
+            types: ["(cities)"],
+            fields: ["geometry", "icon", "name", "place_id"],
+        }
+    
+        gAutocomplete = new google.maps.places.Autocomplete(document.getElementById("query-location"), options);
+    
+        gAutocomplete.addListener('place_changed', handleUpdateAutocomplete);
+}
+
+
+function initPlaces() {
+    // Init map for PlacesServices to work
+    infowindow = new google.maps.InfoWindow();
+    let map = new google.maps.Map(document.getElementById('map'), {});
+    gPlaces = new google.maps.places.PlacesService(map);
+}
+
+
+function askForUserLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition)
+    }
+}
+
+function showPosition(position) {
+    deviceLocation.lat = position.coords.latitude;
+    deviceLocation.lng = position.coords.longitude;
+}
