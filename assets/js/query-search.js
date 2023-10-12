11de31b0f48806f4f09fa18bc247f232930b8c8d
nathangero <nathangero@yahoo.com>
2023-10-12 11:33:14 -0700

    implement showing restaurants on front page

--- assets/js/query-search.js
+++ assets/js/query-search.js
@@ -1,5 +1,6 @@
 const GOOGLE = "AIzaSyCFTg8yxhfKfqvVhtZpfmTyXco9qlHLm2Q";
 const SEARCH_RESULTS = "restaurantResults";
+const SHOW_INITIAL_RESTAURANTS = 4; // Determines how many restaurants to show on the front page
 
 var queryItem = $("#query-item");
 var queryLocation = $("#query-location");
@@ -52,7 +53,7 @@ function fetchGooglePlaces(keyword) {
         type: ['food']
     };
 
-    console.log("request:", request)
+    // console.log("request:", request)
 
     // Use nearbySearch to get results from the user's keyword(s)
     gPlaces.nearbySearch(request, function(results, status) {
@@ -71,18 +72,116 @@ function fetchGooglePlaces(keyword) {
         
         // Store results in local storage to bring to see-more-restaurants.html
         let stringifyResults = JSON.stringify(results);
-        console.log(stringifyResults)
+        // console.log(stringifyResults)
         localStorage.setItem(SEARCH_RESULTS, stringifyResults);
+        displayResults(results);
 
         // This is only here for testing purposes. This will occur when the "See more restaurants" button is clicked
-        window.location.href = "./see-more-restaurants.html"
-        queryItem.val(""); // Clear the input fields after going to the next page
-        queryLocation.val("");
+        // window.location.href = "./see-more-restaurants.html"
+        // queryItem.val(""); // Clear the input fields after going to the next page
+        // queryLocation.val("");
     });
 }
 
 
+function displayResults(results) {
 
+    var restaurantContainer = $("#restaurant-display");
+    restaurantContainer.val("");
+    let cardDiv = $("<div>");
+    cardDiv.addClass("columns p-3 mb-0");
+
+    for (let i = 0; i < SHOW_INITIAL_RESTAURANTS; i++) {
+        let info = results[i];
+        let name = info.name;
+        let isOpen = info.opening_hours.open_now ? "Open" : "Closed";
+        let priceLevel = buildPriceLevelStr(info.price_level);
+        let rating = info.rating;
+        let ratingsCount = info.user_ratings_total;
+
+        let card = $("<div>");
+        card.addClass("card column is-4 mx-3 mt-3")
+
+
+        let cardHeader = $("<div>");
+        cardHeader.addClass("card-header");
+
+        let cardTitle = $("<h3>");
+        cardTitle.addClass("card-header-title title is-3 is-centered");
+        cardTitle.text(name)
+
+        cardHeader.append(cardTitle);
+
+
+        let cardContent = $("<div>");
+        cardContent.addClass("card-content is-size-4");
+
+        let isOpenEl = $("<p>");
+        isOpenEl.addClass("content");
+        isOpenEl.html(`is <strong>${isOpen}</strong>`)
+
+        let ratingEl = $("<p>");
+        ratingEl.addClass("content");
+        ratingEl.html(`<strong>${rating}</strong> /5 (${ratingsCount} total reviews)`)
+
+        let priceLevelEl = $("<p>");
+        priceLevelEl.addClass("content");
+        priceLevelEl.html(priceLevel);
+
+        // let descriptionEl = $("<p>");
+
+
+        cardContent.append(isOpenEl, priceLevelEl, ratingEl);
+        card.append(cardHeader, cardContent);
+        cardDiv.append(card);
+    }
+
+    restaurantContainer.append(cardDiv);
+
+    // var resultColumn = $("<div>").addClass("column is-3 resultDisplay");
+    // var resultCard = $("<div>").addClass("card");
+    // var cardImage = $("<div>").addClass("card-image");
+    // var figure = $("<figure>").addClass("image is-4by3");
+    // var cardContent = $("<div>").addClass("card-content");
+    // var mediaContent = $("<div>").addClass("media-content");
+    // var cardTitle = $("<h1>").addClass("title is-4");
+    // var cardSub = $("<h2>").addClass("subtitle is-6");
+    // var recipeBox = $("<ul>");
+    // resultColumn.append(resultCard);
+    // $(".recipeDisplay").append(resultColumn);
+    // resultCard.append(cardImage);
+    // cardImage.append(figure);
+    // resultCard.append(cardContent);
+    // cardContent.append(mediaContent);
+    // mediaContent.append(cardTitle, cardSub, recipeBox);
+    // recipeBox.append("<li>  this is a test  </li>");
+    // recipeBox.attr("class","ingredient")
+    // figure.append($("<img>").attr("src", imagehtml));
+    // cardTitle.text(title);
+    // cardSub.text("Cuisine type: " + subtitle);
+}
+
+function buildPriceLevelStr(priceLevel) {
+    switch (priceLevel) {
+        case 0:
+            return "Free"
+        
+        case 1:
+            return "<strong>$ </strong>"
+        
+        case 2:
+            return "<strong>$ $</strong>"
+    
+        case 3:
+            return "<strong>$ $ $</strong>"
+
+        case 4:
+            return "<strong>$ $ $ $</strong>"
+        
+        default:
+            return "<strong>$ $</strong>"
+    }
+}
 
 
 
