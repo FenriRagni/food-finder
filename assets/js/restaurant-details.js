5abf02d85256b127db7e2c98f3346beec522131d
FenriRagni <145153161+FenriRagni@users.noreply.github.com>
2023-10-12 19:37:21 -0400

    created anchors in bookmarks to go to more details on fav restaurants

---
+++ assets/js/restaurant-details.js
@@ -0,0 +1,9 @@
+function getRestaurantInfo() {
+    var queryResult = document.location.search.split("=")[1];
+    if (queryResult) {
+        getSearchResults(queryResult);
+    } else {
+        // If no result, go back to main page
+        document.location.replace("./index.html");
+    }
+};
