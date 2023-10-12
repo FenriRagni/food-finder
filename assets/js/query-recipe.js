5e87d562a6e8fe8b0be5bb2afdcae41eb2c3f649
Minami Itsukaichi <mitukaichi@gmail.com>
2023-10-12 14:49:21 -0700

    replace placeholder content in the search results with actual content

--- assets/js/query-recipe.js
+++ assets/js/query-recipe.js
@@ -36,19 +36,21 @@ function showRecipeResults(searchQuery) {
                     var extractCuiseType = data.hits[i].recipe.cuisineType;
                     crusinetype = extractCuiseType[0];      //<----------------RECIPE CUISINE TYPE SOURCE
                     var imageSouce = data.hits[i].recipe.images.SMALL.url;   //<-------- RECIPE IMAGE SOURCE
-                    var ingredient = data.hits[i].recipe.ingredientLines;
-                    // ALL WE HAVE TO DO IS INSERT INTO THE CARD GENERATOR FUNCTION VALUES RETURNED FROM API
-                    RecipecardGenerator(recipeTitle, crusinetype, imageSouce);
+                    var mealTypeData = data.hits[i].recipe.mealType;
+                    var calorieData = Math.round(data.hits[i].recipe.calories);
+                    var recipeId = data.hits[i].recipe.uri.split("_")[1];
+                    // INSERT INTO THE CARD GENERATOR FUNCTION VALUES RETURNED FROM API
+                    RecipecardGenerator(recipeTitle, crusinetype, imageSouce, recipeId, mealTypeData, calorieData);
                 }
             };
+            if (data.hits.length > 0) {
+                $(".recipeDisplay").append('<div> <p class = "is-size-2 mb-3 has-text-centered"><a href = "https://fenriragni.github.io/food-finder/see-more-recipes.html?q=' + searchQuery +'">See more recipes <p></div>');
+            }
         });
-
-
-
 
     // THIS FUNCTION WILL GENERATE ELEMENT ON THE PAGE WE JUST NEED TO NEST THE INFO WE NEED INSIDE
     // THIS FUNCTION WILL TAKE IN TITLE, CARD TEXT CONTENT AND IMAGE URL
-    function RecipecardGenerator(title, subtitle, imagehtml) {
+    function RecipecardGenerator(title, crusinetype, imagehtml, recipeId, mealTypeData, calorieData) {
         var resultColumn = $("<div>").addClass("column is-12 resultDisplay");
         var resultCard = $("<div>").addClass("card");
         var cardImage = $("<div>").addClass("card-image");
@@ -56,7 +58,6 @@ function showRecipeResults(searchQuery) {
         var cardContent = $("<div>").addClass("card-content");
         var mediaContent = $("<div>").addClass("media-content");
         var cardTitle = $("<h1>").addClass("title is-4");
-        var cardSub = $("<h2>").addClass("subtitle is-6");
         var recipeBox = $("<ul>");
         resultColumn.append(resultCard);
         $(".recipeDisplay").append(resultColumn);
@@ -64,12 +65,14 @@ function showRecipeResults(searchQuery) {
         cardImage.append(figure);
         resultCard.append(cardContent);
         cardContent.append(mediaContent);
-        mediaContent.append(cardTitle, cardSub, recipeBox);
-        recipeBox.append("<li>  this is a test  </li>");
+        mediaContent.append(cardTitle,recipeBox);
+        recipeBox.append("<li><b>Type</b> :" + crusinetype + "</li>");
+        recipeBox.append("<li><b>Good for</b> :" + mealTypeData + "</li>");
+        recipeBox.append("<li>" +  calorieData + " calories</li>");
+        recipeBox.append('<a href = "https://fenriragni.github.io/food-finder/recipe-details.html?=' + recipeId + '"> Details</a>');
         recipeBox.attr("class","ingredient")
         figure.append($("<img>").attr("src", imagehtml));
         cardTitle.text(title);
-        cardSub.text("Cuisine type: " + subtitle);
     }
 
 }
